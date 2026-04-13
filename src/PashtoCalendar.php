18ab2da0dd4f742bc75231affa059919db19e1af
Abdul Qadir Nasrat <abdulqadirnasrat483@gmail.com>
2026-04-13 08:05:27 -0700

    file added

--- src/PashtoCalendar.php
+++ src/PashtoCalendar.php
@@ -1,5 +1,73 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Qadir\PashtoCalendar;
 
-class Skeleton {}
+use Qadir\PashtoCalendar\Events\EventManager;
+use Qadir\PashtoCalendar\Converter\GregorianToPashto;
+use Carbon\Carbon;
+use DateTime;
+
+class PashtoCalendar
+{
+    /**
+     * Convert Gregorian input to PashtoDate
+     */
+    public static function fromGregorian($date): PashtoDate
+    {
+        $converter = new GregorianToPashto();
+        return $converter->convert($date);
+    }
+
+    /**
+     * Get current date
+     */
+   public static function now(): PashtoDate
+{
+    return self::fromGregorian(
+        \Carbon\Carbon::now('Asia/Kabul')
+    );
+}
+
+    /**
+     * Parse different formats
+     */
+    public static function parse($date): PashtoDate
+    {
+        if ($date instanceof Carbon || $date instanceof DateTime) {
+            return self::fromGregorian($date);
+        }
+
+        if (is_int($date)) {
+            return self::fromGregorian($date);
+        }
+
+        if (is_string($date)) {
+            return self::fromGregorian(Carbon::parse($date));
+        }
+
+        return self::fromGregorian(Carbon::now());
+    }
+
+    /**
+     * 🚀 MAIN CALENDAR ENGINE (FIXED)
+     */
+ public static function make(int $year, int $month): array
+{
+    $engine = new \Qadir\PashtoCalendar\PashtoDate($year, $month, 1);
+    $days = $engine->generateDays();
+
+    foreach ($days as $index => $day) {
+
+        if (!empty($day['empty'])) {
+            continue;
+        }
+
+        $events = EventManager::forDate($year, $month, $day['day']);
+
+        $days[$index]['events'] = $events;
+        $days[$index]['event_count'] = $events->count();
+    }
+
+    return $days;
+}
+}
